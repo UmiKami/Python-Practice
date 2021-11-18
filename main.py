ea732447811b19c020353e82762f7585bde422ef
UmiKami <ggoku02@gmail.com>
2021-11-18 10:22:27 -0500

    User year of birth input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 print("Hello World of Practice")
 
 fruitList = ["apples", "oranges", "tomatos"]
@@ -6,4 +8,11 @@ for fruits in fruitList:
 
 sum = 2 + 3
 
-print("2+3=" + str(sum))
+print("2 + 3 = " + str(sum))
+
+print("User Input Practice\n-----------------------------\n-----------------------------\n")
+currentYear = date.today()
+date = currentYear.strftime("%Y")
+userAge = input("Enter your age: ")
+yearBorn = int(date) - int(userAge)
+print("If you are " + str(userAge) + ", then you were most likely born on " + str(yearBorn))
